25644e1cc6322766022ad365534c205dc0581590
zsmester <zsolt.mester67@gmail.com>
2016-11-02 20:39:09 +0100

    Solving lesson 2 project part 4

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -23,8 +23,9 @@ from sklearn import svm
 #clf = svm.SVC(kernel="linear")
 clf = svm.SVC(kernel="rbf", C=10000)
 
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
+# to speed up the training time
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
 
 t0 = time()
 clf.fit( features_train, labels_train )
@@ -33,6 +34,11 @@ print "training time:", round(time()-t0, 3), "s"
 t0 = time()
 pred = clf.predict( features_test )
 print "prediction time:", round(time()-t0, 3), "s"
+
+#print pred[10]
+#print pred[26]
+#print pred[50]
+#print (pred == 1).sum()
 
 from sklearn.metrics import accuracy_score
 print accuracy_score(pred, labels_test)
