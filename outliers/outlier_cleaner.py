1e44ff90a5517009b8a8cd78e6e0a9fa8a47e438
zsmester <zsolt.mester67@gmail.com>
2017-01-21 20:54:39 +0100

    Do outliners mini-project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,23 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
-    ### your code goes here
+    splitIndex = int(len(predictions) * 0.9)
 
-    
+    for i in range( 0, splitIndex ):
+        cleaned_data.append( (ages[i], net_worths[i], abs(net_worths[i]-predictions[i])) )
+
+    for i in range( splitIndex, len(predictions) ):
+        cleaned_data.append( (ages[i], net_worths[i], abs(net_worths[i]-predictions[i])) )
+        maxValue = max( cleaned_data, key=lambda item: item[2] )[2]
+        for j in range( 0, len(cleaned_data) ):
+            if cleaned_data[j][2] == maxValue:
+                del cleaned_data[j]
+                break
+
     return cleaned_data
-
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,15 +26,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
-
-
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print 'coef_: ', reg.coef_
+print 'score with test data: ', reg.score(ages_test, net_worths_test)
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -52,10 +48,6 @@ try:
 except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
-
-
-
-
 
 
 
@@ -82,3 +74,5 @@ if len(cleaned_data) > 0:
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
 
+print 'new coef_: ', reg.coef_
+print 'new score with test data: ', reg.score(ages_test, net_worths_test)
