21f76ea849a7a8a5cb1b00f43976b5ff2e1056b1
zsmester <zsolt.mester67@gmail.com>
2017-02-05 22:31:19 +0100

    Do feature selection mini-project.

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,7 +8,7 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
+words_file = "../text_learning/your_word_data.pkl"
 authors_file = "../text_learning/your_email_authors.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
@@ -26,18 +26,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
-features_test  = vectorizer.transform(features_test).toarray()
+features_test  = vectorizer.transform(features_test)
 
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
-features_train = features_train[:150].toarray()
+features_train = features_train[:150]
 labels_train   = labels_train[:150]
 
 
 
 ### your code goes here
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
 
+for i in range(0, len(clf.feature_importances_)):
+    if float(clf.feature_importances_[i]) > float(0.2):
+        print i, ": ", clf.feature_importances_[i], ": ", vectorizer.get_feature_names()[i]
+
+#print vectorizer.get_feature_names()[33614]
+#print vectorizer.get_feature_names()[14343]
